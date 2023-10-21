dd1caae50451b7bcd415c8b93a0191de9679c9bf
thanakorn <thanakorn_w@mfec.co.th>
2023-10-21 22:11:50 +0700

    change css name

--- mangement_booking.php
+++ mangement_booking.php
@@ -9,7 +9,7 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
-    <link rel="stylesheet" type="text/css"  href="./style/booking.css">
+    <link rel="stylesheet" type="text/css"  href="./style/mangement_booking.css">
     <title>Document</title>
     <title>
         <style>
