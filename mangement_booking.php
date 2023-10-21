c8b47131cb448046f7d9513f72ec9cbfca3a9b4b
thanakorn <thanakorn_w@mfec.co.th>
2023-10-21 23:26:14 +0700

    add manage book

--- mangement_booking.php
+++ mangement_booking.php
@@ -32,6 +32,7 @@
             })
         });
         </script>
+        
 </head>
 <body>
    
@@ -62,10 +63,10 @@
                         <h2>ประเภทที่พัก</h2>
                     </div>
                     <div class="top-body">
-                        <button type="button"  class="btn-room " id = "btn-condo">คอนโด / อพาร์ตเมนต์</button>
-                        <button type="button"  class="btn-room move-right" id="btn-kalo">บังกะโล</button>
-                        <button type="button"  class="btn-room move-right" id="btn-single-house">บ้านเดียว</button>
-                        <button type="button"  class="btn-room move-right" id ="btn-villa">วิลลา</button>
+                        <button type="button"  class="btn-room edit " id = "btn-condo" >คอนโด / อพาร์ตเมนต์</button>
+                        <button type="button"  class="btn-room move-right" id="btn-kalo" >บังกะโล</button>
+                        <button type="button"  class="btn-room move-right" id="btn-single-house" >บ้านเดียว</button>
+                        <button type="button"  class="btn-room move-right" id ="btn-villa" >วิลลา</button>
                         <!-- <button class="toggle-button active" id="btn1">แก้ไขข้อมูลที่พัก</button>
                         <button class="toggle-button" id="btn2">เพิ่มข้อมูลที่พัก</button> -->
                     </div>
@@ -132,14 +133,13 @@
                 <hr color="gray">
                 <div class="test" id = "test"></div>
                 <a style="color: #59A3B4;"  href="" id = "addRoom">เพิ่มห้องพักประเภทอื่น</a>
-                //done
+                
                 <div class="box-address">
                     <h2>ชื่อที่พัก</h2>
                     <div class="input-group flex-nowrap">
                         <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="addon-wrapping">
                     </div>
                     <h2>ตำแหน่งที่ตั้ง</h2>
-                    // done
                     <div class="group-address">
                         <div class="address">
                             <label>ที่อยู่</label><br/>
@@ -188,10 +188,10 @@
                         <h2>ประเภทที่พัก</h2>
                     </div>
                     <div class="top-body">
-                        <button type="button" class="btn-room ">คอนโด / อพาร์ตเมนต์</button>
-                        <button type="button"  class="btn-room move-right">บังกะโล</button>
-                        <button type="button"  class="btn-room move-right">บ้านเดียว</button>
-                        <button type="button"  class="btn-room move-right">วิลลา</button>
+                        <button type="button"  class="btn-room " id ="btn-condo-add" name = "condo">คอนโด / อพาร์ตเมนต์</button>
+                        <button type="button"  class="btn-room move-right" id="btn-kalo-add">บังกะโล</button>
+                        <button type="button"  class="btn-room move-right" id="btn-single-house-add">บ้านเดียว</button>
+                        <button type="button"  class="btn-room move-right" id ="btn-villa-add">วิลลา</button>
                     </div>
                     <div class="detail-room">
                         <div class="title-detail">
@@ -259,14 +259,14 @@
                 <hr color="gray">
                 <div class="test" id = "test"></div>
                 <a style="color: #59A3B4;"  href="" id = "addRoom">เพิ่มห้องพักประเภทอื่น</a>
-                //done
+               
                 <div class="box-address">
                     <h2>ชื่อที่พัก</h2>
                     <div class="input-group flex-nowrap">
                         <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="addon-wrapping">
                     </div>
                     <h2>ตำแหน่งที่ตั้ง</h2>
-                    // done
+                   
                     <div class="group-address">
                         <div class="address">
                             <label>ที่อยู่</label><br/>
@@ -319,13 +319,39 @@
         </form>
     </div> -->
     <script>
+        //   function test(name)
+        //     {
+        //     // const btnVila = document.getElementById("btn-villa");
+        //     // const btnCondo = document.getElementById("btn-condo");
+        //     // const btnKalo = document.getElementById("btn-kalo");
+        //     // const btnSingleHome = document.getElementById("btn-single-house");
+        //         console.log("test1 = ",name)
+        //         // const btnActive = document.getElementById(name);
+        //         // if(name == btnCondo)
+        //         // {
+        //         // btnKalo.classList.remove("active")
+        //         // btnSingleHome.classList.remove("active");
+        //         // btnVila.classList.remove("active");
+        //         // btnCondo.classList.add("active")
+        //         // }
+        //     }
         $(document).ready(function() {
-            const btn1 = document.getElementById("btn1");
-            const btn2 = document.getElementById("btn2");
+            
+            const btnEdit = document.getElementById("btn1");
+            const btnAdd = document.getElementById("btn2");
             const btnVila = document.getElementById("btn-villa");
             const btnCondo = document.getElementById("btn-condo");
             const btnKalo = document.getElementById("btn-kalo");
             const btnSingleHome = document.getElementById("btn-single-house");
+            const btnVilaAdd = document.getElementById("btn-villa-add");
+            const btnCondoAdd = document.getElementById("btn-condo-add");
+            const btnKaloAdd = document.getElementById("btn-kalo-add");
+            const btnSingleHomeAdd = document.getElementById("btn-single-house-add");
+            btnKalo.classList.remove("active")
+            btnSingleHome.classList.remove("active");
+            btnVila.classList.remove("active");
+            btnCondo.classList.remove("active")
+           
 
             var countBedRoom = 0; // Initialize countBedRoom to 0
             var singleRoom = 0; // Initialize singleRoom to 0
@@ -334,29 +360,49 @@
             var count = 0;
             var oldButtonId = "";
             $(".body-add-room").hide()
-            $(btn1).click(function(){
-                btn1.classList.add("active");
-                btn2.classList.remove("active");
+            $(btnEdit).click(function(){
+                btnEdit.classList.add("active");
+                btnAdd.classList.remove("active");
                 // btn1.style.display = "none";
-                btn2.style.display = "inline-block";
+                btnAdd.style.display = "inline-block";
+                btnKalo.classList.remove("active")
+                btnSingleHome.classList.remove("active");
+                btnVila.classList.remove("active");
+                btnCondo.classList.remove("active")
                 $(".body").show()
                 $(".body-add-room").hide()
               
               
             });
-            $(btn2).click(function(){
-                btn1.classList.remove("active");
-                btn2.classList.add("active");
-                btn1.style.display = "inline-block";
+            $(btnAdd).click(function(){
+                btnEdit.classList.remove("active");
+                btnAdd.classList.add("active");
+                btnEdit.style.display = "inline-block";
                 $(".body").hide()
                 $(".body-add-room").show()
-
+                btnKaloAdd.classList.remove("active")
+                btnSingleHomeAdd.classList.remove("active");
+                btnVilaAdd.classList.remove("active");
+                btnCondoAdd.classList.remove("active")
             });
             $(btnCondo).click(function(){
                 btnKalo.classList.remove("active")
                 btnSingleHome.classList.remove("active");
                 btnVila.classList.remove("active");
                 btnCondo.classList.add("active")
+                var name = $("#btn-condo").attr("name");
+                console.log("  btnCondo.getAttributeNames(); =", name);
+              
+
+            });
+            $(btnCondoAdd).click(function(){
+                btnKaloAdd.classList.remove("active")
+                btnSingleHomeAdd.classList.remove("active");
+                btnVilaAdd.classList.remove("active");
+                btnCondoAdd.classList.add("active")
+                // var name = $("#btn-condo").attr("name");
+                // console.log("  btnCondo.getAttributeNames(); =", name);
+              
 
             });
             $(btnKalo).click(function(){
@@ -366,6 +412,13 @@
                 btnKalo.classList.add("active")
 
             });
+            $(btnKaloAdd).click(function(){
+                btnSingleHomeAdd.classList.remove("active");
+                btnVilaAdd.classList.remove("active");
+                btnCondoAdd.classList.remove("active");
+                btnKaloAdd.classList.add("active")
+
+            });
             $(btnSingleHome).click(function(){
                 btnKalo.classList.remove("active");
                 btnVila.classList.remove("active");
@@ -373,11 +426,25 @@
                 btnSingleHome.classList.add("active")
 
             });
+            $(btnSingleHomeAdd).click(function(){
+                btnKaloAdd.classList.remove("active");
+                btnVilaAdd.classList.remove("active");
+                btnCondoAdd.classList.remove("active");
+                btnSingleHomeAdd.classList.add("active")
+
+            });
             $(btnVila).click(function(){
                 btnCondo.classList.remove("active")
                 btnKalo.classList.remove("active")
                 btnSingleHome.classList.remove("active");
                 btnVila.classList.add("active");
+
+            });
+            $(btnVilaAdd).click(function(){
+                btnCondoAdd.classList.remove("active")
+                btnKaloAdd.classList.remove("active")
+                btnSingleHomeAdd.classList.remove("active");
+                btnVilaAdd.classList.add("active");
 
             });
 
